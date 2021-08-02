4bb9bea76cfdf44acb05eb412d22e63a650067ee
SaimaAlam <Saimasalam882@yahoo.com>
2021-08-01 21:54:51 -0400

    hi this my commit

--- src/test/java/citi/StepDef/AfterActionClass.java
+++ src/test/java/citi/StepDef/AfterActionClass.java
@@ -1,5 +1,34 @@
 package citi.StepDef;
 
-public class AfterActionClass {
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
+import freeCRM.utilities.DriverSetUp;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+
+public class AfterActionClass {
+	@After
+	public void afterScenarioActions(Scenario scenario) {
+
+		System.out.println(">> Scenario Execution Completed: " + scenario.getName());
+
+		try {
+
+			if (scenario.isFailed()) {
+
+				String screenShotName = scenario.getName().replaceAll(" ", "_");
+				TakesScreenshot ts = (TakesScreenshot) DriverSetUp.chromeDriver;
+				byte[] screenShot = ts.getScreenshotAs(OutputType.BYTES);
+				scenario.attach(screenShot, "image/png", screenShotName);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(">> Closing Chrome Brwoser");
+		DriverSetUp.quiteChromeDriver();
+
+	}
 }
